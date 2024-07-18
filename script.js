6c3320b56549814c1895b7556575ef2f9380544b
mohamed561 <mrajawi561@gmail.com>
2024-07-18 08:16:55 -0700

    fixing some errors

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create remove button
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
         removeButton.onclick = function() {
             taskList.removeChild(li);
         };
